e5b3fe1acb540836029def3f6a0a2038321cd81a
jundoh <jundo1302@gmail.com>
2022-01-20 18:46:57 +0900

    feat: change fetch API to useQuery

--- crypto-tracker/src/App.tsx
+++ crypto-tracker/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Router from "./Router";
 import { createGlobalStyle } from "styled-components";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -81,6 +82,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
